601dffbca489343841b25060b1e6a2854fc051d4
radith10 <radith23ti@mahasiswa.pcr.ac.id>
2025-06-19 22:18:46 +0700

    19/06 22.18 [Route]Halaman Laporan

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,28 @@ const GuestLayout = React.lazy(() => import("./layouts/GuestLayout"));
 
 const MainLayout = React.lazy(() => import("./layouts/MainLayout"));
 const Dashboard = React.lazy(() => import("./pages/Dashbord"));
+const Laporan = React.lazy(() => import("./pages/Laporan"));
 const Pemasukan = React.lazy(() => import("./pages/Pemasukan"));
 const Pengeluaran = React.lazy(() => import("./pages/Pengeluaran"));
 const Budgeting = React.lazy(() => import("./pages/Budgeting"));
 
+const Loading = React.lazy(() => import("./components/Loading"));
+
 export default function App() {
   return (
-    // <Suspense fallback={<Loading />}>
-    <Routes>
-      <Route path="/main" element={<MainLayout />}>
-        <Route path="/main/Dashboard" element={<Dashboard />} />
-        <Route path="/main/Dashboard/Pemasukan" element={<Pemasukan />} />
-        <Route path="/main/Dashboard/Pengeluaran" element={<Pengeluaran />} />
-        <Route path="/main/Dashboard/Budgeting" element={<Budgeting />} />
-      </Route>
-    </Routes>
-    // </Suspense>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/main" element={<MainLayout />}>
+          <Route path="/main/Dashboard" element={<Dashboard />} />
+          <Route path="/main/Laporan" element={<Laporan />} />
+
+          <Route path="/main/Dashboard/Pemasukan" element={<Pemasukan />} />
+          <Route path="/main/Dashboard/Pengeluaran" element={<Pengeluaran />} />
+          <Route path="/main/Dashboard/Budgeting" element={<Budgeting />} />
+        </Route>
+
+        
+      </Routes>
+    </Suspense>
   );
 }
