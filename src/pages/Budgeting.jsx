8e85075a5ac35624a57ec56ed9139ed95a66c81d
rizalwhyu00 <rizal23ti@mahasiswa.pcr.ac.id>
2025-07-16 15:23:07 +0700

    Perubahan tampilan di herosection

--- src/pages/Budgeting.jsx
+++ src/pages/Budgeting.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { AlertTriangle, Wallet, CalendarCheck, RotateCcw } from "lucide-react";
-import supabase from "../lib/supabase";
+
 
 export default function Budgeting() {
   const [budget, setBudget] = useState("");
